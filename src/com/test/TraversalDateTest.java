35745a2dfcf555b40a9a48ecc3ad1a970ebc7035
lsxpkni1989512 <295174190@qq.com>
2019-06-17 16:02:15 +0800

    完成版

---
+++ src/com/test/TraversalDateTest.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import com.test.utils.TimeUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TraversalDateTest {
+    //遍历任意月份的日期
+    public static void main(String[] args) {
+        System.out.println("开始");
+        String month = "2019-02";
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM");
+        try {
+            Date parse = dateFormat.parse(month);
+            TimeUtils.dayReport(parse);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println("结束");
+
+    }
+}
---
+++ src/com/test/utils/TimeUtils.java
@@ -0,0 +1,40 @@
+package com.test.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class TimeUtils {
+    //获取指定月份的天数
+    public static int getDaysByYearMonth(int year, int month) {
+
+        Calendar a = Calendar.getInstance();
+        a.set(Calendar.YEAR, year);
+        a.set(Calendar.MONTH, month - 1);
+        a.set(Calendar.DATE, 1);
+        a.roll(Calendar.DATE, -1);
+        int maxDate = a.get(Calendar.DATE);
+        System.out.println(maxDate);
+        return maxDate;
+    }
+
+    public static void dayReport(Date month) {
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(month);//month 为指定月份任意日期
+        int year = cal.get(Calendar.YEAR);
+        int m = cal.get(Calendar.MONTH);
+        int mm = cal.get(Calendar.DAY_OF_MONTH);
+        System.out.println(mm);//当前月的第几天
+        int dayNumOfMonth = TimeUtils.getDaysByYearMonth(year, m+1);//当钱前月一共有多少天
+//        System.out.println(year +","+(m+1)+","+dayNumOfMonth);
+        cal.set(Calendar.DAY_OF_MONTH, 1);// 从一号开始
+
+        for (int i = 0; i < dayNumOfMonth; i++, cal.add(Calendar.DATE, 1)) {
+            Date d = cal.getTime();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            String df = simpleDateFormat.format(d);
+            System.out.println(df);
+        }
+    }
+}
