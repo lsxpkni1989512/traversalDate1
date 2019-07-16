022bcff45d335512ede94deac191aceb367cd8a0
lsxpkni1989512 <295174190@qq.com>
2019-07-16 15:28:21 +0800

    多线程加锁和不加锁的区别

---
+++ src/com/test/ThreadTest.java
@@ -0,0 +1,78 @@
+package com.test;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ThreadTest {
+    private volatile static int count = 0;
+
+    public static Lock lock = new ReentrantLock();
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        //每个线程让count自增100次
+                        for (int j = 0; j < 100; j++) {
+                            add();
+                        }
+                    }finally {
+
+                    }
+
+                }
+            }).start();
+        }
+
+        try{
+            Thread.sleep(6000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName()+"  最终count："+count);
+    }
+    public static synchronized void add(){
+//        lock.lock();
+        try{
+            count++;
+            System.out.println(Thread.currentThread().getName()+"  count:"+count);
+            Thread.sleep(10);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+//            lock.unlock();
+        }
+//        System.out.println(Thread.currentThread().getName()+"  count:"+count);
+    }
+
+    /**
+     * case1:add()不加syncronized
+     * 出现问题：
+     * Thread-0  count:170
+     * Thread-1  count:170
+     * Thread-0  count:171
+     * Thread-1  count:172
+     * Thread-0  count:173
+     * Thread-1  count:174
+     * main  最终count：174
+     */
+
+    /**
+     * case2:add()加sync
+     * Thread-1  count:194
+     * Thread-1  count:195
+     * Thread-1  count:196
+     * Thread-1  count:197
+     * Thread-1  count:198
+     * Thread-0  count:199
+     * Thread-0  count:200
+     * main  最终count：200
+     */
+
+    /**
+     * case3:
+     */
+
+
+}
