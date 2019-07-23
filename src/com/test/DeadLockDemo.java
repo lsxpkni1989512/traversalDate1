2a304773571bbbee72d162af2d2605b8541c24e1
lsxpkni1989512 <295174190@qq.com>
2019-07-23 10:50:55 +0800

    死锁的例子  （java找问题的命令 jps -l ; jstack 进程号）

---
+++ src/com/test/DeadLockDemo.java
@@ -0,0 +1,34 @@
+package com.test;
+
+class HoldLockThread implements Runnable{
+
+
+    private String lockA;
+    private String lockB;
+
+    @Override
+    public void run() {
+        synchronized (lockA){
+            System.out.println(Thread.currentThread().getName()+"\t 持有锁"+lockA+" 尝试获取锁"+lockB);
+            synchronized (lockB){
+                System.out.println(Thread.currentThread().getName()+"\t 持有锁"+lockB+" 尝试获取锁"+lockA);
+
+            }
+        }
+    }
+    public HoldLockThread(String lockA,String lockB){
+        this.lockA = lockA;
+        this.lockB = lockB;
+    }
+}
+public class DeadLockDemo {
+    public static void main(String[] args) {
+
+        String lockA = "lockA";
+        String lockB = "lockB";
+        new Thread(new HoldLockThread(lockA,lockB),"A").start();
+        new Thread(new HoldLockThread(lockB,lockA),"B").start();
+
+    }
+
+}
